f4d2bc4ac9e1c4a61e57e44e689ee1490d9392f0
Xuwei Li <code@xuwei.li>
2021-07-01 23:57:25 +0800

    User root path / as schemas index page. Use Response.redirect() instead of fetch() logic to get schema.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import schemasIndex from '../schema/index.json'
+// import schemasJsText from '../schema/compile/schemas.cjs.txt'
+
 export default {
   async fetch(request, env) {
     const Url = new URL(request.url)
@@ -6,23 +9,54 @@ export default {
     // Handle root request
 
     if (Url.pathname == '/') {
-      return new Response('V4EX', { status: 200 })
+      const schemasKey = Object.keys(schemasIndex)
+      let html = '<ul>'
+      schemasKey.forEach(key => {
+        let $id = (new URL(key, 'https://schema.v4ex.com')).href
+        html += `<li><a href="${$id}" target="_blank">${$id}</a> -> ${schemasIndex[key]}</li>`
+      })
+      html += '</ul>'
+
+      return new Response(html, {
+        status: 200,
+        headers: {
+          "content-type": "text/html;charset=UTF-8"
+        }
+      })
     }
+
+
+    // ========================================================================
+    // Handle schemas.js file request
+
+    // if (Url.pathname == '/schemas.js') {
+      
+      // const event = {}
+      // event.request = request
+      // const options = {}
+      // const schemasJsFile = await getAssetFromKV(event, options)
+
+      // console.log(env)
+
+    //   return new Response(schemasJsText, { status: 200 })
+    // }
+
 
     // ========================================================================
     // Handle schema request
 
     const schema = Url.pathname.split('/')[1]
-    switch (schema) {
-      case 'mining-task': {
-        return fetch('https://v4ex-coin.v4ex.workers.dev/schema/mining-task')
-      }
-      case 'mining-task-work': {
-        return fetch('https://v4ex-coin.v4ex.workers.dev/schema/mining-task-work')
-      }
-      default: {
-        return new Response('V4EX', { status: 200 })
-      }
+    if (schemasIndex.hasOwnProperty(schema)) {
+      // BUG
+      // Too many redirects
+      // return fetch(schemasIndex[schema])
+      return Response.redirect(schemasIndex[schema], 301)
     }
+
+
+    // ========================================================================
+    // Fallback response
+
+    return new Response('V4EX', { status: 200 })
   },
 }
